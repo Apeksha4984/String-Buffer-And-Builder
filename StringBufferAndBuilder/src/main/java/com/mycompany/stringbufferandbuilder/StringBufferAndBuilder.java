bc8e87789917423bedfcaad8695a75600f4e272b
Apeksha Rane <121625467+Apeksha4984@users.noreply.github.com>
2024-04-22 10:16:14 +0530

    Add files via upload

---
+++ StringBufferAndBuilder/src/main/java/com/mycompany/stringbufferandbuilder/StringBufferAndBuilder.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.stringbufferandbuilder;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class StringBufferAndBuilder {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ StringBufferAndBuilder/src/main/java/com/mycompany/stringbufferandbuilder/StringBuilder.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.stringbufferandbuilder;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class StringBuilder {
+     public static void main(String[] args){
+        StringBuffer sb1 = new StringBuffer("apeksha");
+        StringBuffer sb2 = new StringBuffer("apeksha");
+//         System.out.println(sb1.equals(sb2));
+//         System.out.println(sb1.capacity());
+//         System.out.println(sb1.append("hello world"));
+//         System.out.println(sb1.insert(1, 'n'));
+//         System.out.println(sb1.delete(2, 3));
+           System.out.println(sb1.length());   
+    }
+    
+    
+}
